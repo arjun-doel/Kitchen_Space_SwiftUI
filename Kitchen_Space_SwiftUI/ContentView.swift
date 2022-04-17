02ec31a5ad4b45b6fbdbae2401317bdf590b4754
arjundoel <arjundoel@gmail.com>
2022-04-17 10:01:13 +0100

    Initial Commit

---
+++ Kitchen_Space_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kitchen_Space_SwiftUI
+//
+//  Created by Arjun Doel on 17/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kitchen_Space_SwiftUI/Kitchen_Space_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kitchen_Space_SwiftUIApp.swift
+//  Kitchen_Space_SwiftUI
+//
+//  Created by Arjun Doel on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Kitchen_Space_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
